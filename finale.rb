e7b0dc6d92e6a5170f8d6138c2d172d82c2347e6
Andy Shen <andy@shenie.net>
2013-07-13 13:38:57 +1000

    ready to submit

--- finale.rb
+++ finale.rb
@@ -8,6 +8,41 @@ class Array
   end
 end
 
+class Elevator
+  attr_accessor :id, :floor, :available
+  alias :available? :available
+
+  def initialize(id, floor)
+    @id    = id
+    @floor = floor.to_i
+    @available = true
+  end
+
+  def distance_to(floor)
+    (self.floor - floor).abs
+  end
+end
+
+class Request
+  attr_accessor :elevator, :to_floor, :wait_time
+
+  def initialize(request_id, to_floor, wait_time)
+    @request_id = request_id
+    @to_floor   = to_floor.to_i
+    @wait_time  = wait_time.to_i
+  end
+
+  def service_by(elevator)
+    @elevator = elevator
+    elevator.available = false
+  end
+
+  def <=>(another)
+    another.wait_time <=> self.wait_time
+  end
+
+end
+
 #### answer library above
 
 class Finale
@@ -30,7 +65,32 @@ class Finale
     
 #### answer below
 
+  elevators = []
+  input().first.split(',').each_with_index do |floor, id|
+    elevators << Elevator.new(id, floor)
+  end
 
+  requests = []
+  input().last.split(";").each_with_index do |str, idx|
+    (floor, wait_time) = str.split(',')
+    requests << Request.new(idx, floor, wait_time)
+  end
+
+  requests.sort.each do |request|
+    available_elevators = elevators.find_all { |e| e.available? }
+    available_elevators.each { |e| debug "req to_floor=#{request.to_floor} wait #{request.wait_time} e=#{e.id} e-floor=#{e.floor}}"}
+
+    available_elevators_by_distance = available_elevators.map { |e| [e.distance_to(request.to_floor), e] }.sort
+    available_elevators_by_distance.each {|pair| debug "dist=#{pair.first} e.id=#{pair.last.id} e.floor=#{pair.last.floor}"}
+
+    next_available_elevator = available_elevators_by_distance.first.last
+    # next_available_elevator = available_elevators_by_distance.find { |(dist, e)| dist != 0 }.last
+    debug "found e #{next_available_elevator.id}"
+
+    request.service_by next_available_elevator
+  end
+
+  output << requests.map { |r| r.elevator.id }.join(',')
 
 #### answer above
 
--- spec/finale_spec.rb
+++ spec/finale_spec.rb
@@ -5,19 +5,37 @@ require './finale'
 
 describe Finale do
 
+  it "test order" do
+    req1 = Request.new 1, 1, 10
+    req2 = Request.new 1, 1, 30
+    req3 = Request.new 1, 1, 20
+
+    [req1, req2, req3].sort.should == [req2, req3, req1]
+  end
+
   it "input 1" do
-    input = ""
-    Finale.new(input, false).result.should == ""
+    input = ["0,1,19,9,56", "13,15;67,60;68,35;"]
+    Finale.new(input, false).result.should == "3,4,2"
   end
   
   it "input 2" do
-    input = ""
-    Finale.new(input, false).result.should == ""
+    input = ["45,10,7,1,0", "27,45;11,10;"]
+    Finale.new(input, false).result.should == "1,2"
   end
 
   it "input 3" do
-    input = ""
-    Finale.new(input, false).result.should == ""
+    input = ["98,99,26,5,75", "27,45;11,10;5,100;"]
+    Finale.new(input, false).result.should == "2,4,3"
+  end
+
+  it "input 4" do
+    input = ["1,2,3,4,5", "9,10;11,12;13,14;"]
+    Finale.new(input, false).result.should == "2,3,4"
+  end
+
+  it "input 5" do
+    input = ["1,2,3,4,5", "7,60;"]
+    Finale.new(input, false).result.should == "4"
   end
 
 end
